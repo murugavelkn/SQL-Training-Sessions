491fa002954983905c7b4e6bdef7bf84014dc427
Murugavel <murugavel.kn@gmail.com>
2021-10-18 13:34:58 +0400

    NEW table

---
+++ 3-DQL-Language/03-1-Simple-Query/simple-query.sql
@@ -0,0 +1,52 @@
+-- Syntax
+SELECT <clause>, Aggregatefun()/CalculativeColumn AS <Alias Name>
+FROM <clause>
+{WHERE <clause> | HAVING <Condition> -- for aggregatefunction to filter }
+{ORDER BY Columnname1, columnname2,...[ASC/DESC]}   -- Called as sorting asc :=  Min to Max;  Desc : = Max to Min
+{GROUP BY ColunmName2, ColumnName2,...} -- can be use when aggregate function involves
+										-- COUNT(), SUM(),MAX(),MIN().AVG(),...
+										-- with colunmName1,columName2...
+
+
+--/*------------------------------------------------------------------------
+SELECT <columnname1, columname2, columnname3......
+FROM <TABLE NAME>
+WHERE <Conditions>
+
+conditions := condition1 + condition2 + condition3
+??  AND / OR
+
+--/*----------------------
+SELECT SalesOrderID, SalesOrderDetailID, ProductID, OrderQty
+FROM [Sales].[SalesOrderDetail]
+
+
+-- AND Condition
+SELECT *
+FROM [Sales].[SalesOrderDetail]
+WHERE ProductID = 712 AND orderQty > 4 AND orderQty <=8 AND ModifiedDate = '2012-06-30'
+
+
+-- BETWEEN
+SELECT *
+FROM [Sales].[SalesOrderDetail]
+WHERE ProductId = 712 AND ORDERQty BETWEEN 4 AND 8 AND ModifiedDate BETWEEN '2012-01-01' AND '2012-03-15'
+
+
+-- OR
+SELECT *
+FROM [Sales].[SalesOrderDetail]
+WHERE ProductID = 712 OR ProductID = 711
+
+-- IN
+
+SELECT *
+FROM [Sales].[SalesOrderDetail]
+WHERE ProductID  IN (711,712)
+
+
+--NOT IN
+
+SELECT *
+FROM [Sales].[SalesOrderDetail]
+WHERE ProductID NOT IN (711,712)
