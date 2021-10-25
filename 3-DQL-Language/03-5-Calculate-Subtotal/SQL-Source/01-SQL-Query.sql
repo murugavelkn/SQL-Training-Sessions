3a9a93c54c8e5fc8ee44f80ee2e05ae92b84c46c
Murugavel <murugavel.kn@gmail.com>
2021-10-25 14:45:10 +0400

    Calculate Subtotal in SQL Query

--- 3-DQL-Language/03-5-Calculate-Subtotal/SQL-Source/01-SQL-Query.sql
+++ 3-DQL-Language/03-5-Calculate-Subtotal/SQL-Source/01-SQL-Query.sql
@@ -14,3 +14,125 @@ INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'Ju
 INSERT INTO SalesList(SalesMonth,SalesQuartes,SalesYear,SalesTotal) VALUES (N'March','Q1',2020,170)
 GO
 SELECT  * FROM SalesList
+
+-- Example-1
+SELECT  SalesYear, SUM(SalesTotal) AS SalesTotal FROM SalesList GROUP BY ROLLUP(SalesYear)
+
+--Example-2
+SELECT  SalesYear,SalesQuartes, SUM(SalesTotal) AS SalesTotal
+    FROM SalesList GROUP BY ROLLUP(SalesYear, SalesQuartes)
+
+--Example-3
+SELECT  SalesYear,SalesQuartes,SalesMonth ,SUM(SalesTotal) AS SalesTotal
+FROM SalesList GROUP BY ROLLUP(SalesYear, SalesQuartes, SalesMonth)
+
+--Understanding GROUPING function
+SELECT SalesYear,
+SalesQuartes,
+SUM(SalesTotal) AS SalesTotal ,
+GROUPING(SalesQuartes) AS SalesQuarterGrp,
+GROUPING(SalesYear) AS SYearGrp
+FROM SalesList
+GROUP BY ROLLUP(SalesYear, SalesQuartes)
+
+
+SELECT
+CASE
+WHEN GROUPING(SalesQuartes)=1 AND GROUPING(SalesYear)=0
+THEN 'SubTotal'
+WHEN GROUPING(SalesQuartes)=1 AND GROUPING(SalesYear)=1
+THEN
+'Grand Total'
+ELSE
+CAST(SalesYear AS varchar(10))
+END
+AS SalesYear,
+SalesQuartes,
+SUM(SalesTotal) AS SalesTotal
+FROM SalesList
+GROUP BY ROLLUP(SalesYear,SalesQuartes)
+
+
+--Calculate subtotal in SQL query only for one column
+SELECT SalesMonth,SalesTotal ,
+ROW_NUMBER() OVER(ORDER BY NEWID()) AS RowNumber FROM SalesList
+
+----------------------------------------------------------------------------
+WITH CTE AS (
+SELECT SalesMonth,SalesTotal ,
+ROW_NUMBER() OVER(ORDER BY NEWID())
+AS RowNumber FROM SalesList
+)
+
+SELECT
+    RowNumber ,SalesMonth,SUM(SalesTotal) AS SalesTotal
+FROM CTE
+GROUP BY ROLLUP(SalesMonth, RowNumber)
+
+----------------------------------------------------------------------------
+WITH CTE AS (
+SELECT SalesMonth,SalesTotal ,
+ROW_NUMBER() OVER(ORDER BY NEWID())
+AS RowNumber FROM SalesList
+)
+
+SELECT
+CASE WHEN GROUPING(RowNumber) =1 THEN 'SubTotal'
+ELSE
+
+    SalesMonth
+    END AS SalesMonth,SUM(SalesTotal) AS SalesTotal
+FROM CTE
+GROUP BY ROLLUP(SalesMonth, RowNumber)
+
+----------------------------------------------------------------------------
+WITH CTE AS (
+SELECT SalesMonth,SalesTotal ,
+ROW_NUMBER() OVER(ORDER BY NEWID())
+AS RowNumber FROM SalesList
+)
+
+SELECT
+CASE WHEN GROUPING(RowNumber) =1 THEN 'SubTotal'
+ELSE
+
+    SalesMonth
+    END AS SalesMonth,SUM(SalesTotal) AS SalesTotal
+FROM CTE
+GROUP BY ROLLUP(SalesMonth, RowNumber)
+HAVING GROUPING(SalesMonth) = 0
+
+----------------------------------------------------------------------------
+
+--Using GROUPING SET extension as an alternative method
+SELECT NULL AS SalesQuarter, SalesMonth,
+SUM(SalesTotal) AS SalesTotal
+FROM  SalesList
+GROUP BY SalesMonth
+UNION ALL
+    SELECT  SalesQuartes, NULL AS SalesMonth,
+SUM(SalesTotal) AS SalesTotal
+FROM  SalesList
+GROUP BY SalesQuartes
+
+----------------------------------------------------------------------------
+
+SELECT
+SalesQuartes,SalesMonth ,
+SUM(SalesTotal) AS SalesTotal
+FROM SalesList
+    GROUP BY GROUPING SETS(SalesQuartes,SalesMonth)
+
+----------------------------------------------------------------------------
+SELECT
+  CASE  WHEN GROUPING(SalesQuartes)=1 AND GROUPING(SalesYear)=0  THEN
+            'SubTotal'
+        WHEN GROUPING(SalesQuartes)=1 AND GROUPING(SalesYear)=1  THEN
+            'Grand Total'
+  ELSE
+      CAST(SalesYear AS varchar(10))
+  END AS SalesYear,
+  SalesQuartes,
+  SUM(SalesTotal) AS SalesTotal
+FROM SalesList
+GROUP BY GROUPING SETS(SalesYear,(SalesYear,SalesQuartes),())  
